56c003c104b9afd38a3878f172a438febc2af6e0
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-08-26 18:46:55 -0400

    Update set-timezone.sh

--- set-timezone.sh
+++ set-timezone.sh
@@ -2,36 +2,32 @@
 
 # List of patterns to search for
 patterns=(
-  "Africa" "America" "Antarctica" "Arctic" "Asia" "Atlantic"
-  "Australia" "Brazil" "Canada" "Chile" "Cuba" "Egypt" "Eire"
-  "Europe" "GB" "GB-Eire" "GMT" "GMT0" "GMT+0" "GMT-0" "Greenwich"
-  "HST" "HongKong" "Iceland" "Indian" "Iran" "Israel" "Jamaica"
-  "Japan" "Kwajalein" "Libya" "Mexico" "Navajo" "Pacific" "Poland"
-  "Portugal" "Singapore" "Turkey" "UCT" "US" "UTC" "Zulu"
+  "Africa/*" "America/*" "Antarctica/*" "Arctic/*" "Asia/*" "Atlantic/*"
+  "Australia/*" "Brazil/*" "Canada/*" "Chile/*" "Cuba" "Egypt" "Eire"
+  "Europe/*" "GB" "GB-Eire" "GMT" "GMT0" "GMT+0" "GMT-0" "Greenwich"
+  "HST" "HongKong" "Iceland" "Indian/*" "Iran" "Israel" "Jamaica"
+  "Japan" "Kwajalein" "Libya" "Mexico/*" "Navajo" "Pacific/*" "Poland"
+  "Portugal" "Singapore" "Turkey" "UCT" "US/*" "UTC" "Zulu"
 )
 
-# Function to list timezones with numbering
-list_timezones() {
+# Function to get a list of timezones
+get_timezones() {
   local count=1
   for pattern in "${patterns[@]}"; do
-    echo "Matching timezones for pattern: $pattern"
     find /usr/share/zoneinfo -type f -path "/usr/share/zoneinfo/$pattern" | sed 's|/usr/share/zoneinfo/||' | awk -v cnt=$count '{print cnt". "$0; cnt++}'
   done
 }
 
-# List timezones
-list_timezones
-
-# Prompt user for selection
-echo -ne "\nEnter the number corresponding to your timezone choice: "
-read -r choice
-
-# Validate user input
-timezone=$(find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | awk "NR==$choice")
-if [ -z "$timezone" ]; then
-  echo "Invalid selection. Exiting."
-  exit 1
-fi
+# Get timezones and display options
+echo "Select a timezone from the list:"
+PS3="Enter the number corresponding to your timezone choice: "
+select timezone in $(get_timezones); do
+  if [ -n "$timezone" ]; then
+    break
+  else
+    echo "Invalid selection. Please try again."
+  fi
+done
 
 # Set timezone
 echo "Setting timezone to $timezone"
