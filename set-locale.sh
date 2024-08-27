3fe86f716c029773f0c657813826e74595c3b943
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-08-26 20:53:18 -0400

    Update set-locale.sh

--- set-locale.sh
+++ set-locale.sh
@@ -2,7 +2,7 @@
 
 # Function to get a list of locales from /etc/locale.gen
 get_locales() {
-  cat /etc/locale.gen | awk '{print NR ". " $1}'
+  awk '{print NR ". " $1}' /etc/locale.gen
 }
 
 # Collect locales into an array
@@ -56,4 +56,46 @@ while true; do
   display_page $start $end
 
   # Prompt user for selection or continue
-  echo -ne "Enter the number of your locale choice from this p
+  echo -ne "Enter the number of your locale choice from this page, or press Enter to see more locales: "
+  read -r choice
+
+  # Check if user made a choice
+  if [[ "$choice" =~ ^[0-9]+$ ]]; then
+    if [[ "$choice" -ge 1 && "$choice" -le $total_locales ]]; then
+      # Extract the selected locale
+      selected_locale=$(echo "${locales[$((choice-1))]}" | awk '{print $2}')
+
+      # Check if the selected locale is commented
+      if [[ "$selected_locale" == \#* ]]; then
+        # Remove the leading '#' for uncommenting
+        uncommented_locale=$(echo "$selected_locale" | sed 's/^# //')
+        echo "Uncommenting locale: $uncommented_locale"
+        sed -i "/^# $uncommented_locale/s/^# //" /etc/locale.gen
+      else
+        echo "Selected locale is already active."
+      fi
+
+      # Run locale-gen to apply the changes
+      locale-gen
+
+      # Set the locale in /etc/locale.conf
+      echo "Setting locale to $selected_locale"
+      echo "LANG=$selected_locale" > /etc/locale.conf
+
+      # Verify locale setting
+      echo "Locale has been set to $(cat /etc/locale.conf)"
+      break
+    else
+      echo "Invalid selection. Please enter a valid number from the displayed list."
+    fi
+  elif [[ -z "$choice" ]]; then
+    # Continue to the next page
+    if ((end == total_locales)); then
+      echo "No more locales to display."
+      break
+    fi
+    current_page=$((current_page + 1))
+  else
+    echo "Invalid input. Please enter a number or press Enter to continue."
+  fi
+done
