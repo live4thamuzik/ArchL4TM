eafafe4ea3e0b165064e6087e252428af2cccc43
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-09-23 23:58:55 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -742,7 +742,7 @@ install_aur_helper() {
     local aur_helper="$1"
     local repo_url="$2"
     local temp_dir="/mnt/tmp/$aur_helper"
-}
+
     echo "Installing $aur_helper"
 
     # Add the temporary user to the wheel group (needed for Yay)
@@ -757,34 +757,34 @@ install_aur_helper() {
         fi 
 
         # Build and install the AUR helper (with --noconfirm)
-        cd "$temp_dir" && makepkg -si --noconfirm || {
+        cd "$temp_dir" && makepkg -si --noconfirm || {  
             echo "Failed to build and install $aur_helper. Check the installation logs for more details."
-            exit 1
-        } '
+            exit 1 
+        }
 
         # Clean up
         cd ~ && rm -rf "$temp_dir"
         echo "$aur_helper installed successfully! You can now use $aur_helper to install packages from the AUR."
+    '
 }
 
 
 # Ask the user which AUR helper they want
 options=("Yay" "Paru")
 select aur_helper in "${options[@]}"; do
-    case $aur_helper in
-        "Yay")
-            install_aur_helper "Yay" "https://aur.archlinux.org/yay.git"
-            ;;
-        "Paru")
-            install_aur_helper "Paru" "https://aur.archlinux.org/paru.git"
-            ;;
-        *) echo "Invalid option";;
-    esac
+    case $aur_helper in
+        "Yay")
+            install_aur_helper "Yay" "https://aur.archlinux.org/yay.git"
+            ;;
+        "Paru")
+            install_aur_helper "Paru" "https://aur.archlinux.org/paru.git"
+            ;;
+        *) echo "Invalid option";;
+    esac
 done
 
 # Remove the temporary user
 userdel -r temp_aur_user
-
 # Select GUI (Optional) 
 echo -ne "
 +-----------------------+
