4dbcd47a902a1c4e8c903efece5a796a177930e0
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-09-24 09:03:43 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -777,7 +777,8 @@ select aur_helper in "${options[@]}"; do
             install_aur_helper "Yay" "https://aur.archlinux.org/yay.git"
             ;;
         "Paru")
-            install_aur_helper "Paru" "https://aur.archlinux.org/paru.git"
+            # Explicitly provide the repo URL for Paru
+            install_aur_helper "Paru" "https://aur.archlinux.org/paru.git" 
             ;;
         *) echo "Invalid option";;
     esac
@@ -785,6 +786,7 @@ done
 
 # Remove the temporary user
 userdel -r temp_aur_user
+
 # Select GUI (Optional) 
 echo -ne "
 +-----------------------+
