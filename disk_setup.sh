295c690c9a5e4467aa1f0a9c2c807fa6f93376da
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-10-12 16:37:18 -0400

    Update disk_setup.sh

--- disk_setup.sh
+++ disk_setup.sh
@@ -95,60 +95,6 @@ setup_lvm() {
         exit 1
     fi
 
-setup_lvm() {
-    local disk="$1"
-    local password="$2"  # Pass the encryption password as an argument
-
-    log_output "Setting up LVM on disk: $disk"
-
-    # Format partitions 
-    if ! mkfs.fat -F32 "${disk}1" || \
-       ! mkfs.ext4 "${disk}2"; then
-        log_error "Failed to format partitions" $?
-        exit 1
-    fi
-
-    # Setup encryption on partition 3 using LUKS
-    if ! echo "$password" | cryptsetup luksFormat "${disk}3"; then
-        log_error "Failed to format LUKS partition" $?
-        exit 1
-    fi
-
-    # Open LUKS partition
-    if ! echo "$password" | cryptsetup open --type luks --batch-mode "${disk}3" lvm; then
-        log_error "Failed to open LUKS partition" $?
-        exit 1
-    fi
-
-    # Create physical volume for LVM on partition 3 with data alignment 1m
-    if ! pvcreate /dev/mapper/lvm; then
-        log_error "Failed to create physical volume" $?
-        exit 1
-    fi
-
-    # Create volume group called volgroup0 on partition 3
-    if ! vgcreate volgroup0 /dev/mapper/lvm; then
-        log_error "Failed to create volume group" $?
-        exit 1
-    fi
-
-    # Get logical volume sizes from the user
-    get_lv_sizes() {
-        read -p "Enter root logical volume size (e.g., 50G, 200G): " root_lv_size
-        # You might want to add validation here for $root_lv_size
-        export ROOT_LV_SIZE="$root_lv_size"
-        log_output "Root logical volume size: $ROOT_LV_SIZE"
-    }
-
-    get_lv_sizes
-
-    # Create logical volumes (lv_home will use remaining space)
-    if ! lvcreate -L "$ROOT_LV_SIZE" volgroup0 -n lv_root || \
-       ! lvcreate -l 100%FREE volgroup0 -n lv_home; then
-        log_error "Failed to create logical volumes" $?
-        exit 1
-    fi
-
     # Load kernel module
     modprobe dm_mod
 
