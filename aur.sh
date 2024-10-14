1b1ebebfef01f18f89288d01e7a5363a7fd37a77
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-10-14 14:53:12 -0400

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-source functions.sh
+source ./functions.sh  # Source functions.sh from the current directory
 
 aur_helper="$1"
+
+# Create a temporary directory
+temp_dir=$(mktemp -d)
 
 case "$aur_helper" in
     yay)
         log_output "Installing yay..."
         if ! pacman -Sy --noconfirm --needed base-devel git go || \
-           ! runuser -u nobody git clone https://aur.archlinux.org/yay.git /tmp/yay || \
-           ! chown nobody:nobody /tmp/yay || \
-           ! cd /tmp/yay || \
+           ! runuser -u nobody git clone https://aur.archlinux.org/yay.git "$temp_dir/yay" || \
+           ! chown nobody:nobody "$temp_dir/yay" || \
+           ! cd "$temp_dir/yay" || \
            ! runuser -u nobody makepkg -si --noconfirm || \
-           ! rm -rf /tmp/yay; then
+           ! rm -rf "$temp_dir"; then
             log_error "Failed to install yay" $?
             exit 1
         fi
@@ -20,11 +23,11 @@ case "$aur_helper" in
     paru)
         log_output "Installing paru..."
         if ! pacman -Sy --noconfirm --needed base-devel git rust cargo || \
-           ! runuser -u nobody git clone https://aur.archlinux.org/paru.git /tmp/paru || \
-           ! chown nobody:nobody /tmp/paru || \
-           ! cd /tmp/paru || \
+           ! runuser -u nobody git clone https://aur.archlinux.org/paru.git "$temp_dir/paru" || \
+           ! chown nobody:nobody "$temp_dir/paru" || \
+           ! cd "$temp_dir/paru" || \
            ! runuser -u nobody makepkg -si --noconfirm || \
-           ! rm -rf /tmp/paru; then
+           ! rm -rf "$temp_dir"; then
             log_error "Failed to install paru" $?
             exit 1
         fi
