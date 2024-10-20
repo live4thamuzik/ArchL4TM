443b1df1ec057fc4924aed5e6482d8cd57077786
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-10-20 16:12:53 -0400

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export hostname="$HOSTNAME"
+export username="$USERNAME"
+export userpass="$USER_PASSWORD"
+export rootpass="$ROOT_PASSWORD"
+export timezone="$timezone"
+export locale="$locale"
+export keymap="$keymap"
+export keymap="$GUI_CHOICE"
+export keymap="$AUR_HELPER
