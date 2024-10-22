ab7531e8d7f6eda0763b04808b29313695945c67
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-10-22 10:03:27 -0400

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -10,56 +10,38 @@ arch-chroot /mnt /bin/bash
 source /global_functions.sh
 
 # --- Pacman Configuration ---
-echo -ne "Configuring pacman"
 configure_pacman
 
-echo -ne "Installing microcode"
+# --- Install microcode ---
 install_microcode 
 
 # --- Install Additional Packages ---
-echo -ne "Installing packages"
 install_additional_packages
 
 # --- Enable services --- 
-echo -ne "Enabling services"
 enable_services
 
 # --- Essential System Setup ---
-echo -ne "Setting locale"
 set_locale
-
-echo -ne "Update intiramfs"
 update_initramfs
 
 # --- User and Hostname Configuration ---
-echo -ne "Creating user"
 create_user "$USERNAME"
-
-echo -ne "Setting passwords"
 set_passwords "$USERNAME" "$USER_PASSWORD" "$ROOT_PASSWORD"
-
-echo -ne "Setting hostname"
 set_hostname "$HOSTNAME"
-
-echo -ne "Updating Sudoers"
 update_sudoers
 
 # --- Bootloader ---
-echo -ne "Installing GRUB"
 install_grub
-
-echo -ne "Configuring GRUB"
 configure_grub "$DISK"
 
-echo -ne "Looking for NVIDIA GPU, DKMS drivers will be installed if GPU found."
+# --- Install NVIDA drivers if GPU found ---
 install_nvidia_drivers
 
 # --- GUI Installation ---
-echo -ne "Installing Desktop/Server Environment"
 install_gui "$GUI_CHOICE"
 
 # --- AUR Installation ---
-echo -ne "Installing AUR"
 install_aur_helper "$AUR_HELPER"
 
 # --- Exit Chroot ---
