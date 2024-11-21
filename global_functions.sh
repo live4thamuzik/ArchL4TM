40aff47255d738c8b4c400975ad90fe2c8ed0b54
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-11-20 21:24:39 -0500

    Update global_functions.sh
    added function to handle correct disk/partition names when using NVMe drives rather than SATA

--- global_functions.sh
+++ global_functions.sh
@@ -170,6 +170,20 @@ partition_disk() {
     partprobe "$disk"
 }
 
+get_partitions() {
+    local disk="$1"
+
+    if [[ "$disk" == *nvme* ]]; then
+        export PART1="${disk}p1"
+        export PART2="${disk}p2"
+        export PART3="${disk}p3"
+    else
+        export PART1="${disk}1"
+        export PART2="${disk}2"
+        export PART3="${disk}3"
+    fi
+}
+
 setup_lvm() {
     local disk="$1"
     local password="$2"  # Pass the encryption password as an argument
@@ -177,25 +191,25 @@ setup_lvm() {
     log_output "Setting up LVM on disk: $disk"
 
     # Format EFI partition
-    if ! mkfs.fat -F32 "${disk}1"; then
+    if ! mkfs.fat -F32 "$PART1"; then
         log_error "Failed to format EFI partition" $?
         exit 1
     fi
 
     # Format boot partition
-    if ! mkfs.ext4 "${disk}2"; then
+    if ! mkfs.ext4 "$PART2"; then
         log_error "Failed to format boot partition" $?
         exit 1
     fi
 
     # Setup encryption on partition 3 using LUKS
-    if ! echo "$password" | cryptsetup luksFormat "${disk}3"; then
+    if ! echo "$password" | cryptsetup luksFormat "$PART3"; then
         log_error "Failed to format LUKS partition" $?
         exit 1
     fi
 
     # Open LUKS partition
-    if ! echo "$password" | cryptsetup open --type luks --batch-mode "${disk}3" lvm; then
+    if ! echo "$password" | cryptsetup open --type luks --batch-mode "$PART3" lvm; then
         log_error "Failed to open LUKS partition" $?
         exit 1
     fi
@@ -257,7 +271,7 @@ setup_lvm() {
         log_error "Failed to create /boot directory" $?
         exit 1
     fi
-    if ! mount "${disk}2" /mnt/boot; then
+    if ! mount "$PART2" /mnt/boot; then
         log_error "Failed to mount /boot" $?
         exit 1
     fi
@@ -267,7 +281,7 @@ setup_lvm() {
         log_error "Failed to create /boot/EFI directory" $?
         exit 1
     fi
-    if ! mount "${disk}1" /mnt/boot/EFI; then
+    if ! mount "$PART1" /mnt/boot/EFI; then
         log_error "Failed to mount /boot/EFI" $?
         exit 1
     fi
@@ -562,7 +576,7 @@ configure_grub() {
 
     # Make sure DISK is exported and available in the environment
     if ! sed -i '/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved' /etc/default/grub || \
-       ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice='"$DISK"'3:volgroup0 loglevel=3"' /etc/default/grub || \
+       ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice='"$PART3"':volgroup0 loglevel=3"' /etc/default/grub || \
        ! sed -i '/^#GRUB_ENABLE_CRYPTODISK=y/c\GRUB_ENABLE_CRYPTODISK=y' /etc/default/grub || \
        ! sed -i '/^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true' /etc/default/grub || \
        ! cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale.en.mo || \
@@ -603,7 +617,7 @@ install_nvidia_drivers() {
 
         # Update GRUB configuration with NVIDIA settings
         # Make sure DISK is exported and available in the environment
-        if ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'"$DISK"'3:volgroup0 loglevel=3"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'"$DISK"'3:volgroup0 nvidia_drm_modeset=1 loglevel=3"' /etc/default/grub || \
+        if ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'"$PART3"':volgroup0 loglevel=3"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'"$PART3"':volgroup0 nvidia_drm_modeset=1 loglevel=3"' /etc/default/grub || \
            ! grub-mkconfig -o /boot/grub/grub.cfg; then
             log_error "Failed to update GRUB configuration with NVIDIA settings" $?
             exit 1
