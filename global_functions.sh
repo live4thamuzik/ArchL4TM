07e2f21061a96cf10b8e0e1f17781d4e2b3acdd1
Steve <98226771+live4thamuzik@users.noreply.github.com>
2024-11-23 12:16:27 -0500

    Update global_functions.sh
    fix logic to more accurately find nvme drives

--- global_functions.sh
+++ global_functions.sh
@@ -603,14 +603,14 @@ configure_grub() {
   log_output "Configuring GRUB..."
 
   # Make sure DISK is exported and available in the environment
-  if [[ $disk =~ nvme ]]; then
+  if [[ $DISK == "/dev/nvme"* ]]; then
     PART_PREFIX="p"
   else
     PART_PREFIX=""
     fi
     
     if ! sed -i '/^GRUB_DEFAULT=/c\GRUB_DEFAULT=saved' /etc/default/grub || \
-       ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'${DISK}${PART_PREFIX}'3:volgroup0 loglevel=3"' /etc/default/grub || \
+       ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice='${DISK}${PART_PREFIX}'3:volgroup0 loglevel=3"' /etc/default/grub || \
        ! sed -i '/^#GRUB_ENABLE_CRYPTODISK=y/c\GRUB_ENABLE_CRYPTODISK=y' /etc/default/grub || \
        ! sed -i '/^#GRUB_SAVEDEFAULT=true/c\GRUB_SAVEDEFAULT=true' /etc/default/grub || \
        ! cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale.en.mo || \
@@ -650,12 +650,12 @@ install_nvidia_drivers() {
         fi
 
         # Update GRUB configuration with NVIDIA settings
-        if [[ $DISK =~ nvme ]]; then
+        if [[ $DISK == "/dev/nvme"* ]]; then
           PART_PREFIX="p"
         else
           PART_PREFIX=""
         fi
-            if ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'${DISK}${PART_PREFIX}'3:volgroup0 loglevel=3"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice=\/dev\/'${DISK}${PART_PREFIX}'3:volgroup0 nvidia_drm_modeset=1 loglevel=3"' /etc/default/grub || \
+            if ! sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice='${DISK}${PART_PREFIX}'3:volgroup0 loglevel=3"/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet cryptdevice='${DISK}${PART_PREFIX}'3:volgroup0 nvidia_drm_modeset=1 loglevel=3"' /etc/default/grub || \
                ! grub-mkconfig -o /boot/grub/grub.cfg; then
                 log_error "Failed to update GRUB configuration with NVIDIA settings" $?
             fi
