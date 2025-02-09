1a303e315a2a5d5673778cc4f0854e1109b623d5
Steve <live4thamuzik@gmail.com>
2025-02-09 11:25:06 -0500

    Update global_functions.sh

--- global_functions.sh
+++ global_functions.sh
@@ -204,97 +204,6 @@ get_encryption_password() {
     done
 }
 
-# --- Ask for desired timezone to use ---
-#setup_timezone() {
-#    log_output "Setting up timezone..."
-
-    # Function to get a list of timezones
-#    get_timezones() {
-#        local count=1
-#        find /usr/share/zoneinfo -type f | sed 's|/usr/share/zoneinfo/||' | awk -v cnt=$count '{print cnt". "$0; cnt++}'
-#    }
-
-    # Collect timezones into an array
-#    mapfile -t timezones < <(get_timezones)
-
-    # Check if timezones were collected
-#    if [ ${#timezones[@]} -eq 0 ]; then
-#        log_error "No timezones found. Please check the timezone directory and try again." 1
-#        exit 1
-#    fi
-
-    # Constants
-#    PAGE_SIZE=40
-#    COLS=1  # Number of columns to display
-#    NUMBER_WIDTH=4  # Width for number and dot
-#    COLUMN_WIDTH=2  # Width of each column for timezones
-
-    # Function to display a page of timezones in columns
-#    display_page() {
-#    local start=$1
-#    local end=$2
-#    local count=0
-
-#    echo "Timezones ($((start + 1)) to $end of ${#timezones[@]}):"
-
-#    for ((i=start; i<end; i++)); do
-        # Print timezones in columns with minimized gap
-#        printf "%-${NUMBER_WIDTH}s%-${COLUMN_WIDTH}s" "${timezones[$i]}" ""
-#        count=$((count + 1))
-
-#        if ((count % COLS == 0)); then
-#            echo
-#        fi
-#    done
-    
-#}
-
- # Display pages of timezones
-#    total_timezones=${#timezones[@]}
-#    current_page=0
-
-#    while true; do
-#        start=$((current_page * PAGE_SIZE))
-#        end=$((start + PAGE_SIZE))
-#        if ((end > total_timezones)); then
-#            end=$total_timezones
-#        fi
-
-#        display_page "$start" "$end"
-
-        # Prompt user for selection or continue
-#        read -r -p "Enter the number of your timezone choice from this page, or press Enter to see more timezones: " choice
-
-        # Check if user made a choice
-#        if [[ "$choice" =~ ^[0-9]+$ ]]; then 
-#            if ((choice >= 1 && choice <= total_timezones)); then 
-                # Extract the selected timezone
-#                selected_timezone=$(echo "${timezones[$((choice-1))]}" | awk '{print $2}')
-
-                # Set timezone
-#                ln -sf /usr/share/zoneinfo/"$selected_timezone" /etc/localtime
-                
-                # Sync hardware clock with system clock
-#                hwclock --systohc
-                
-                # Verify timezone setting
-#                log_output "Timezone has been set to $(readlink -f /etc/localtime)"
-#                break  # Exit the loop if a valid selection is made
-#            else
-#                echo "Invalid selection. Please enter a valid number from the displayed list."
-#            fi
-#        elif [[ -z "$choice" ]]; then  # User pressed Enter, go to the next page
-#            if ((end == total_timezones)); then  # If this is the last page, loop back to the beginning
-#                current_page=0
-#            else
-#                current_page=$((current_page + 1))
-#            fi
-#        else
-#            echo "Invalid input. Please enter a number or press Enter to continue."
-#        fi
-#    done
-#}
-
 # --- Timezone Selection ---
 setup_timezone() {
   log_output "Setting up timezone..."
@@ -1104,17 +1013,36 @@ install_gui() {
     "
     if [[ "$GUI_CHOICE" == "hyprland" ]]; then
         # Clone the dotfiles branch
-        git clone --progress --verbose https://github.com/live4thamuzik/hyprdots.git ./hyprdots || {
-            log_error "Failed to hyprdots"
+        git clone --progress --verbose https://github.com/live4thamuzik/L4TM-HyDE.git ./L4TM-HyDE || {
+            log_error "Failed to L4TM-HyDE"
             exit 1
         }
 
         # Copy Configs to /
-        cp -r ./hyprdots/Configs /
+        cd ./L4TM-HyDE/Scripts
 
+     # Temporarily allow the user to run sudo without a password (within the chroot)
+    echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
+    
+        # Switch to the created user and install HyDE
+    if ! runuser -u "$USERNAME" -- /bin/bash -c "
         # Call hypr.sh
-        bash ./hypr.sh
+        if ! bash ./install.sh
+        log_error \"Failed to install HyDE\" \$?
+        exit 1
+    fi
+    "; then
+        log_error "Failed to install AUR packages as $USERNAME" $?
+        # Remove the temporary sudoers entry in case of failure
+        sed -i "/$USERNAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
+        exit 1
+    fi
 
+    # Remove the temporary sudoers entry
+    sed -i "/$USERNAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
+
+    log_output "HyDE installation complete!"
+    
     elif [[ "$GUI_CHOICE" == "gnome" ]]; then
         log_output "Installing GNOME desktop environment..."
 
